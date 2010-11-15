e30c0355fcc63d0be8689bab92037bb70faf7b45
jonlusk123@gmail.com <jonlusk123@gmail.com@9a7dd2ad-1450-2422-52bb-3a4ccd6006e6>
2010-11-15 05:56:52 +0000

    Changed name to EasyInvite Fixed a bug causing the if statement to always be true Removed Debug messages

---
+++ EasyInvite/EasyInvite.lua
@@ -0,0 +1,42 @@
+EasyInvite_Enabled = "disabled"; --"enabled"; --"disabled";
+EasyInvite_Password = "addme";
+
+function EasyInvite() 
+	SLASH_EASYINVITE1, SLASH_EASYINVITE2 = '/ei', '/easyinvite';
+	local function handler(msg, editbox)
+		if msg == 'on' then
+			EasyInvite_Enabled = "enabled";
+		elseif msg == 'off' then
+			EasyInvite_Enabled = "disabled";
+		elseif msg == '' then
+			print("|cffffff00".."EasyInvite by Undone and Elunora".."|r");
+			print("|cffffff00".."/ei on to Enable".."|r");
+			print("|cffffff00".."/ei off to Disable".."|r");
+			print("|cffffff00".."/ei <password> to set invite password.".."|r");
+		else
+			EasyInvite_Password = msg;
+		end
+		print("|cffffff00".."EasyInvite is "..EasyInvite_Enabled.."|r");
+		if (EasyInvite_Enabled == "enabled") then print("|cffffff00".."Password is "..EasyInvite_Password.."|r"); end
+	end
+
+	SlashCmdList["EASYINVITE"] = handler; -- Also a valid assignment strategy
+
+	local function EasyInvite_OnEvent(self,event,arg1,arg2) 
+		if ((not UnitExists("party1") or IsPartyLeader("player")) and (arg1:lower():match(EasyInvite_Password)) and (EasyInvite_Enabled == "enabled")) then
+			if (GetNumPartyMembers() == 4) then 
+				ConvertToRaid();
+			end
+			InviteUnit(arg2);
+			--print("|cffffff00".."Password "..EasyInvite_Password.." Enabled "..EasyInvite_Enabled.."|r");
+		else
+			--print("|cffffff00".."Something went wrong "..GetNumPartyMembers().." "..UnitExists("party1").." "..IsPartyLeader("player").."|r");
+		end
+	end
+
+	local f = CreateFrame("frame") 
+	f:RegisterEvent("CHAT_MSG_WHISPER") 
+	f:SetScript("OnEvent", EasyInvite_OnEvent); --function(self,event,arg1,arg2) 
+
+end
+
