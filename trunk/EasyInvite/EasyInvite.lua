970badb72e3d83e628c994a0aef49ac5785394c5
jonlusk123@gmail.com <jonlusk123@gmail.com@9a7dd2ad-1450-2422-52bb-3a4ccd6006e6>
2010-11-15 07:31:12 +0000

    Now prints password if it has been changed regardless of whether the addon is disabled Minor cosmetic issues

--- trunk/EasyInvite/EasyInvite.lua
+++ trunk/EasyInvite/EasyInvite.lua
@@ -1,13 +1,13 @@
-EasyInvite_Enabled = "disabled"; --"enabled";
+EasyInvite_Enabled = "Disabled"; --"Enabled";
 
 function EasyInvite() 
 	SLASH_EASYINVITE1, SLASH_EASYINVITE2 = '/ei', '/easyinvite';
 	local function handler(msg, editbox)
 		if (EasyInvite_Password == nil) then EasyInvite_Password = "addme"; end
 		if msg == 'on' then
-			EasyInvite_Enabled = "enabled";
+			EasyInvite_Enabled = "Enabled";
 		elseif msg == 'off' then
-			EasyInvite_Enabled = "disabled";
+			EasyInvite_Enabled = "Disabled";
 		elseif msg == '' then
 			print("|cffffff00".."EasyInvite by".."|r".."|cffa37748".." Undone".."|r".."|cffffff00".." and ".."|r".."|cffa37748".."Elunora".."|r");
 			print("|cffffff00".."/ei on to Enable".."|r");
@@ -15,15 +15,16 @@ function EasyInvite()
 			print("|cffffff00".."/ei <password> to set invite password.".."|r");
 		else
 			EasyInvite_Password = msg;
+			if (EasyInvite_Enabled == "Disabled") then print("|cffffff00".."Password is "..EasyInvite_Password.."|r"); end
 		end
 		print("|cffffff00".."EasyInvite is "..EasyInvite_Enabled.."|r");
-		if (EasyInvite_Enabled == "enabled") then print("|cffffff00".."Password is "..EasyInvite_Password.."|r"); end
+		if (EasyInvite_Enabled == "Enabled") then print("|cffffff00".."Password is "..EasyInvite_Password.."|r"); end
 	end
 
 	SlashCmdList["EASYINVITE"] = handler; -- Also a valid assignment strategy
 
 	local function EasyInvite_OnEvent(self,event,arg1,arg2) 
-		if ((not UnitExists("party1") or IsPartyLeader("player")) and (arg1:lower():match(EasyInvite_Password)) and (EasyInvite_Enabled == "enabled")) then
+		if ((not UnitExists("party1") or IsPartyLeader("player")) and (arg1:lower():match(EasyInvite_Password)) and (EasyInvite_Enabled == "Enabled")) then
 			if (GetNumPartyMembers() == 4) then 
 				ConvertToRaid();
 			end
